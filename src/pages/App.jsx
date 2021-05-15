4cf864a7a8f3bdd0224c9f165c819ab114ae6bd4
IvanPenga <ivan.penga2@gmail.com>
2021-05-15 11:12:59 +0200

    init

---
+++ src/pages/App.jsx
@@ -0,0 +1,15 @@
+import { useCallback, useState } from "react";
+import Stream from "../components/stream/Stream"
+
+const App = () => {
+
+
+
+  return (
+    <div className="App">
+      <Stream />
+    </div>
+  );
+}
+
+export default App;
